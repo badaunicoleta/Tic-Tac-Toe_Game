a45da1da79d46ef4d1481d02a73d6a3a6285facc
badaunicoleta <nicoletabadau6@gmail.com>
2022-02-27 16:07:02 +0200

    game commit

---
+++ src/game/Player1.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.*;
+
+public class Player1 {
+    public static String p1name;
+    public void setX(JButton b){
+        b.setText("X");
+    }
+    public Player1(){};
+}
---
+++ src/game/Player2.java
@@ -0,0 +1,14 @@
+package game;
+
+import javax.swing.*;
+
+public class Player2 {
+    private String name;
+    public static String p2name;
+
+    public void set0(JButton b){
+        b.setText("0");
+    }
+
+    public Player2(){};
+}
